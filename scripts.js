22133f23d2325a8bccb523b14924733edb1e509c
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-19 14:24:26 +0600

    the change

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// let boolean1 = true;
+// let boolean2 = false;
+// let examle1 = 5 < 6;//true
+// let examle2 = 5 > 6;//false
+// let examle3 = 5 != 6;//true
+// let examle4 = "5" > 5;//true
+// let examle5 = "Hello"  != "Hello";//false
+// let example6 ="077" > 5;//false
+
+let a = 5;
+let b =7;
+if (a >b){
+    console.log('A is highter than B');
+}
+if (a < b){
+    console.log('B is highter than A');
+}
+if(a == b){
+    console.log('A is equal to B');
+}
